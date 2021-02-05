844cb985a50ab3c87460da88d3369fad71139200
Remi Schwartz <schwartz.remi@gmail.com>
2021-02-05 16:00:22 +0100

    react-redux part from yesterday added

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::MessagesController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_channel
+
+  def index
+    messages = @channel.messages
+    api_messages = messages.map do |message|
+      {
+        id: message.id,
+        author: User.find(message.user_id).email,
+        content: message.content,
+        created_at: message.created_at
+      }
+    end
+    render json: api_messages
+  end
+
+  def create
+  end
+
+  private
+
+  def set_channel
+    @channel = Channel.find_by(name: params[:channel_id])
+  end
+end
